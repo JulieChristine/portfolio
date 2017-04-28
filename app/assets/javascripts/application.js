43c87854e0742fa3888c0d80fb126770727b3af2
JulieChristine <julie.wolsak@gmail.com>
2017-04-28 18:03:39 +0800

    add navbar and header styling including js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,9 @@
 //= require jquery
 //= require bootstrap-sprockets
+
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+	$("header").css({
+		opacity: 0.5 + (scroll/200)
+	});
+});
